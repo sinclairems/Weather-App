cc1d4b8c8074a424d3cae6749b5099b56f6ec99f
Erin <50000677+sinclairems@users.noreply.github.com>
2024-02-02 14:51:29 -0600

    html base, minor updates to other files

---
+++ assets/scripts.js
@@ -0,0 +1,27 @@
+/*
+Notes: 
+var APIKey = "99689e0743da7c7ac47a8052ced29530";
+// Variable to store the user input city name
+var city;
+
+// Construct a query call to make the API call
+OpenWeather API Call:
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL) // need to adjust app to accept user input to store in var city 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
+
+    fetch
+    name
+    insert text content from that data
+    https://jsonplaceholder.typicode.com/
+*/
+
