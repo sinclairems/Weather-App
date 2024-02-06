c56b651278fe9589666811d0bc6481b75e211eb1
Erin <50000677+sinclairems@users.noreply.github.com>
2024-02-06 11:30:11 -0600

    restart - committing a new start to this app with notes

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,12 @@
-/*
-Notes: 
+// Notes from BCS: 
 var APIKey = "99689e0743da7c7ac47a8052ced29530";
 // Variable to store the user input city name
 var city;
 
 // Construct a query call to make the API call
-OpenWeather API Call:
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// OpenWeather API Call:
+// Currently showing Chicago lon and lat and excluding minutely, hourly, and alerts
+var queryURL = "https://api.openweathermap.org/data/3.0/onecall?lat=33.4&lon=-94.04&exclude=minutely,hourly,alerts&appid=99689e0743da7c7ac47a8052ced29530"
 
 fetch(queryURL) // need to adjust app to accept user input to store in var city 
     .then(function (response) {
@@ -19,9 +17,16 @@ fetch(queryURL) // need to adjust app to accept user input to store in var city
     });
 
 
-    fetch
-    name
-    insert text content from that data
-    https://jsonplaceholder.typicode.com/
-*/
+    // fetch
+    // name
+    // insert text content from that data
+    // https://jsonplaceholder.typicode.com/
+
+    // FROM SANDY SMITH
+    // Gather APIs
+    // var weatherUrl = ""
+    // var apiKey= ""
+    // var geoUrl = ""
+
+    // Identify all ids from html
 
